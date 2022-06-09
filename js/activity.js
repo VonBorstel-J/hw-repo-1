612687791da69a20e38db343bc42794b6e74fa55
VonBorstel-J <jordanvonborstel@gmail.com>
2022-06-08 21:27:45 -0700

    finally finished hw week 5 day 1

--- js/activity.js
+++ js/activity.js
@@ -1,18 +1,9 @@
 $(document).ready(function() {
-   
-    $('td').css('cursor', 'pointer');
-
-$("td").click(function () {
-var content = $(this).text()
-     if ( "td".text == "Not Available") { 
-      //No Highlight
-     } else {     
-         $("td").toggleClass ("highlight");
-        };
-
-      });
-  });
-
-
-
- 
+$('td').css('cursor', 'pointer');
+$("td").click(function() {
+    var content = $(this).text(); 
+    if (content != "Not Available"){ 
+        $(this).toggleClass("tdhighlight");
+        }
+    });
+});
